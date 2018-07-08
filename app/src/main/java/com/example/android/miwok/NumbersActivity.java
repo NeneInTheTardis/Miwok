0c6dbe59cae62aa126d435936fb7b824e53f32c8
NeneInTheTARDIS <miyavidechu@hotmail.com>
2018-07-08 23:43:24 +0200

    Added strings.xml and deleted the hardcoded numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,37 +2,29 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.animation.LinearInterpolator;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("one", "lutti"));
-        words.add(new Word("two", "otiiko"));
-        words.add(new Word("three", "tolookosu"));
-        words.add(new Word("four", "oyyisa"));
-        words.add(new Word("five", "massokka"));
-        words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven", "kenekaku"));
-        words.add(new Word("eight", "kawinta"));
-        words.add(new Word("nine", "wo´e"));
-        words.add(new Word("ten", "na´aacha"));
+        words.add(new Word(getString(R.string.number_one), "lutti"));
+        words.add(new Word(getString(R.string.number_two), "otiiko"));
+        words.add(new Word(getString(R.string.number_three), "tolookosu"));
+        words.add(new Word(getString(R.string.number_four), "oyyisa"));
+        words.add(new Word(getString(R.string.number_five), "massokka"));
+        words.add(new Word(getString(R.string.number_six), "temmokka"));
+        words.add(new Word(getString(R.string.number_seven), "kenekaku"));
+        words.add(new Word(getString(R.string.number_eight), "kawinta"));
+        words.add(new Word(getString(R.string.number_nine), "wo´e"));
+        words.add(new Word(getString(R.string.number_ten), "na´aacha"));
 
         WordAdapter itemsAdapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -2,12 +2,32 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word(getString(R.string.number_one), "lutti"));
+        words.add(new Word(getString(R.string.number_two), "otiiko"));
+        words.add(new Word(getString(R.string.number_three), "tolookosu"));
+        words.add(new Word(getString(R.string.number_four), "oyyisa"));
+        words.add(new Word(getString(R.string.number_five), "massokka"));
+        words.add(new Word(getString(R.string.number_six), "temmokka"));
+        words.add(new Word(getString(R.string.number_seven), "kenekaku"));
+        words.add(new Word(getString(R.string.number_eight), "kawinta"));
+        words.add(new Word(getString(R.string.number_nine), "wo´e"));
+        words.add(new Word(getString(R.string.number_ten), "na´aacha"));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
